9ea500e56943cb768b15956f5798578a1b76b7b6
Alberto Anzellotti <alberto.anzellotti@studenti.unitn.it>
2014-03-20 03:12:01 +0100

    main updates, now running ok
    1) defined operational function to simplify future cycle usage
    2) time-funtion-colored plot line and attractors scatters
    3) added basic asint. evaluation and related color
    4) now using a new model (little more simplified but much more properties-following)
    TODO : plot the bitmap

--- three_magnets_pendulum_model.py
+++ three_magnets_pendulum_model.py
@@ -9,31 +9,36 @@
 import numpy as np
 from scipy.integrate import odeint
 from matplotlib.pylab import *
-from matplotlib import pylab
 from math import *
 from mpl_toolkits.mplot3d import Axes3D
 
- 
+
 def p_drop(x_drop, y_drop):
     #standard model constants
     global R, C, Q, D, r, s, a, b, c
-    R=0.1		 #friction
-    C=1		 #gravity lin. approx. self restoring force
-    Q=10000000	 #magnetic approx. charge force
-    D=25	 #length of the pendulum
-    r=20	 #distance of the magnets from the centre. (!smaller than d)
+    R=.01		 #friction
+    C=1*1e-2	 #gravity lin. approx. self restoring force
+    Q=3*1e1	 #magnetic approx. charge force
+    D=100	 #length of the pendulum
+    r=30	 #distance of the magnets from the centre. (!smaller than d)
     class a(object):
         pass
     class b(object):
         pass
     class c(object):
         pass
+    a.name="a"
+    b.name="b"
+    c.name="c"
+    a.color="r"
+    b.color="g"
+    c.color="b"
     c.x=0	 #north point
-    c.y=r 
-    b.x = c.x * 0.5 - ( c.y * 0.866025403784438) #south-east point
-    b.y = c.x * 0.866025403784438 + ( c.y * 0.5)
-    a.x = c.x * 0.5 - ( c.y * (-0.86602540378443)) #south-west point
-    a.y = c.x * (-0.86602540378443) + ( c.y * 0.5)
+    c.y=r
+    b.x = c.x * (-0.49999999999999978) - ( c.y * 0.86602540378443871) #south-east point
+    b.y = c.x * 0.86602540378443871 + ( c.y * (-0.49999999999999978))
+    a.x = c.x *(-0.50000000000000044) - ( c.y * (-0.86602540378443837)) #south-west point
+    a.y = c.x * (-0.86602540378443837) + ( c.y * (-0.50000000000000044))
     #initial conditions
     s = np.array([
     0,# x'(t_0)
@@ -43,37 +48,71 @@ def p_drop(x_drop, y_drop):
     ])
 
 
-#fuuuuk il termine magnetico sommatoria blah e' costante e nulllo?
-def d(s,t):
+def d_backup(s,t):
     return np.array([
     -R*s[0] - C*s[1] + Q* ((a.x-s[1])/np.power(np.power(a.x-s[1],2)+np.power(a.y-s[3],2)+D*D,3/2)+ (b.x-s[1])/np.power(np.power(b.x-s[1],2)+np.power(b.y-s[3],2)+D*D,3/2)+ (c.x-s[1])/np.power(np.power(c.x-s[1],2)+np.power(c.y-s[3],2)+D*D,3/2)),
     s[0],
     -R*s[2] - C*s[3] + Q*( (a.y-s[3])/np.power(np.power(a.x-s[1],2)+np.power(a.y-s[3],2)+D*D,3/2)+ (b.y-s[3])/np.power(np.power(b.x-s[1],2)+np.power(b.y-s[3],2)+D*D,3/2)+ (c.y-s[3])/np.power(np.power(c.x-s[1],2)+np.power(c.y-s[3],2)+D*D,3/2) ),
-    s[2]]) 
+    s[2]])
 
-
-def d1(sx,sy,t):
+def d(s,t):
     return np.array([
-    -R*s[0] - C*s[1] + (a.x-s[1])/np.power(np.power(a.x-s[1],2)+np.power(a.y-s[3],2)+D*D,3/2)+ (b.x-s[1])/np.power(np.power(b.x-s[1],2)+np.power(b.y-s[3],2)+D*D,3/2)+ (c.x-s[1])/np.power(np.power(c.x-s[1],2)+np.power(c.y-s[3],2)+D*D,3/2),
+    -R*s[0] - C*s[1] + Q* ((a.x-s[1])/np.power(np.power(a.x-s[1],2)+np.power(a.y-s[3],2)+1e-12,3/2)+ (b.x-s[1])/np.power(np.power(b.x-s[1],2)+np.power(b.y-s[3],2)+1e-12,3/2)+ (c.x-s[1])/np.power(np.power(c.x-s[1],2)+np.power(c.y-s[3],2)+1e-12,3/2)),
     s[0],
-    -R*s[2] - C*s[3] + (a.y-s[3])/np.power(np.power(a.x-s[1],2)+np.power(a.y-s[3],2)+D*D,3/2)+ (b.y-s[3])/np.power(np.power(b.x-s[1],2)+np.power(b.y-s[3],2)+D*D,3/2)+ (c.y-s[3])/np.power(np.power(c.x-s[1],2)+np.power(c.y-s[3],2)+D*D,3/2) ,
-    s[2]]) 
+    -R*s[2] - C*s[3] + Q*( (a.y-s[3])/np.power(np.power(a.x-s[1],2)+np.power(a.y-s[3],2)+1e-12,3/2)+ (b.y-s[3])/np.power(np.power(b.x-s[1],2)+np.power(b.y-s[3],2)+1e-12,3/2)+ (c.y-s[3])/np.power(np.power(c.x-s[1],2)+np.power(c.y-s[3],2)+1e-12,3/2) ),
+    s[2]])
+
+def solve_oed():
+    global sol_x,sol_y,d,s,t
+    tmax = 100
+    dt = .05
+    t = np.linspace(0, tmax, num=np.round(tmax/dt)+1)
+    _,sol_x,_,sol_y = odeint(d, s, t).T
+
+def plot():
+    global fig, ax, i, cStep, fft_axes
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    i=0
+    cStep=int(0.4/(dt))#step piu' piccolo quando dt piu' grande
+    while i < len(t.tolist()):
+        i
+        ax.plot(xs=sol_x[i:i+cStep], ys=sol_y[i:i+cStep], zs=t[i:i+cStep],  color=plt.cm.jet(0.2+7*(np.sqrt(t.tolist()[i])/max(t.tolist()))))
+        i=i+cStep-1
+    ax.scatter(a.x,a.y,100, color=a.color, marker='.')
+    ax.scatter(b.x,b.y,100, color=b.color, marker='.')
+    ax.scatter(c.x,c.y,100, color=c.color, marker='.')
+    ax.scatter(0, 0, 100, marker='.')
+    xlim([-35,35])
+    ylim([-35,35])
+    plt.show()
+
+def asint():
+    #define asint.
+    ext_asint=np.array([np.mean(sol_x[-100:-1]),np.mean(sol_y[-100:-1])])
+    #find closer
+    a.dist=np.sqrt(np.power(a.x-ext_asint[0],2)+np.power(a.y-ext_asint[1],2))
+    b.dist=np.sqrt(np.power(b.x-ext_asint[0],2)+np.power(b.y-ext_asint[1],2))
+    c.dist=np.sqrt(np.power(c.x-ext_asint[0],2)+np.power(c.y-ext_asint[1],2))
+    objs=np.array([a, b, c])
+    dists=np.array([a.dist, b.dist, c.dist])
+    closer = objs[np.where( dists == dists.min())[0][0]]
+    print "The reached point is "+closer.name
+    print "The associated color is "+closer.color
+    return closer.color
+
+p_drop(30,0 )
+solve_oed()
+plot()
+asint()
 
 
-p_drop(16, -21)
-d(s,0)
-
-tmax = 100
-dt = .0001
-t = np.linspace(0, tmax, num=np.round(tmax/dt)+1)
-'''
+"""MORE PLOTTING
 sol = odeint(d, s, t)
 plt.plot(t, sol, label='lol')
 plt.legend()
 show()
-'''
-_,sol_x,_,sol_y = odeint(d, s, t).T
-'''
+
 plt.plot(sol_x, sol_y, label='X,Y dinamics')
 plt.legend()
 
@@ -86,17 +125,4 @@ show()
 plt.plot(t, sol_y, label='X,Y dinamics')
 plt.legend()
 show()
-'''
-fig = plt.figure()
-ax = Axes3D(fig)
-# put 0s on the y-axis, and put the y axis on the z-axis
-ax.plot(xs=sol_x, ys=sol_y, zs=t, zdir='z', label='xs=x, ys=y, zdir=time')
-circle1=plt.Circle((a.x,a.y,0),.2,color='r')
-fig.gca().add_artist(circle1)
-xlim([-35,35])
-fft_axes.set_autoscalex_on(False)
-ylim([-35,35])
-
-plt.show()
-
-#    fig.savefig('coupled-ode-Python.png')
+"""
