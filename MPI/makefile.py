048433df09287239187e89aabe4c8b721c4359ac
Alberto Anzellotti <alberto.anzellotti@gmail.com>
2014-05-27 22:32:54 +0200

    makefile build global function

--- MPI/makefile.py
+++ MPI/makefile.py
@@ -1,6 +1,53 @@
 import fileinput
 import shutil
 import subprocess
+
+
+
+def build():
+    global all
+    ### duplicate function file
+    shutil.copy2( f_name, f_build )
+
+    #apply changes
+    for line in fileinput.input( f_build, inplace=True ):
+        print(line.replace(C_placeholder, C), end='')
+
+    for line in fileinput.input( f_build, inplace=True ):
+        print(line.replace(Q_placeholder, Q), end='')
+
+    for line in fileinput.input( f_build, inplace=True ):
+        print(line.replace(R_placeholder, R), end='')
+
+    for line in fileinput.input( f_build, inplace=True ):
+        print(line.replace(r_placeholder, r), end='')
+
+    for line in fileinput.input( f_build, inplace=True ):
+        print(line.replace(img_d_placeholder, img_d), end='')
+
+    for line in fileinput.input( f_build, inplace=True ):
+        print(line.replace(D_placeholder, D), end='')
+    #ADD MORE
+
+
+    ### duplicate mpi file
+    shutil.copy2( mpi_name, mpi_build )
+
+    #apply changes
+    for line in fileinput.input( mpi_build, inplace=True ):
+        print(line.replace(mpi_f_placeholder, f_build), end='')
+
+    #compile new mpic++
+    p = subprocess.Popen('mpic++ '+mpi_build+" -o "+mpi_c+" -std=c++11", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in p.stdout.readlines():
+        print(line)
+
+    #see exceptions
+    retval = p.wait()
+    print (retval)
+
+##### GENERAL VALUES
+
 f_name="doTheMath.cpp" #name of the virgin function script
 mpi_name="MPIbasejump.cpp" #name of the virgin mpi launcher file
 
@@ -28,43 +75,6 @@ Q="30"
 img_d="1.0"
 D="40"
 
-### duplicate function file
-shutil.copy2( f_name, f_build )
+build()
 
-#apply changes
-for line in fileinput.input( f_build, inplace=True ):
-    print(line.replace(C_placeholder, C), end='')
-
-for line in fileinput.input( f_build, inplace=True ):
-    print(line.replace(Q_placeholder, Q), end='')
-
-for line in fileinput.input( f_build, inplace=True ):
-    print(line.replace(R_placeholder, R), end='')
-
-for line in fileinput.input( f_build, inplace=True ):
-    print(line.replace(r_placeholder, r), end='')
-
-for line in fileinput.input( f_build, inplace=True ):
-    print(line.replace(img_d_placeholder, img_d), end='')
-
-for line in fileinput.input( f_build, inplace=True ):
-    print(line.replace(D_placeholder, D), end='')
-#ADD MORE
-
-
-### duplicate mpi file
-shutil.copy2( mpi_name, mpi_build )
-
-#apply changes
-for line in fileinput.input( mpi_build, inplace=True ):
-    print(line.replace(mpi_f_placeholder, f_build), end='')
-
-#compile new mpic++
-p = subprocess.Popen('mpic++ '+mpi_build+" -o "+mpi_c+" -std=c++11", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-for line in p.stdout.readlines():
-    print(line)
-
-#see exceptions
-retval = p.wait()
-print (retval)
-
+##### Build 2 ...
